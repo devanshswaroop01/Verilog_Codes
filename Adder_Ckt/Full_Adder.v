099581fb62cb7a8b64c0ea7577ef4cdf4922870d
devanshswaroop01 <153737673+devanshswaroop01@users.noreply.github.com>
2025-09-01 22:24:34 +0530

    Update Full_Adder.v

--- Adder_Ckt/Full_Adder.v
+++ Adder_Ckt/Full_Adder.v
@@ -1,9 +1,10 @@
 //Full Adder 
 module full_adder(a, b, cin, sum, cout);
-  input a, b, cin;
-  output sum, cout;
+input a, b, cin;
+output sum, cout;
 assign sum = a ^ b ^ cin;
-  assign cout = (a & b) | (b & cin) | (cin & a);
+assign cout = (a & b) | (b & cin) | (cin & a);
 endmodule
+
 
 
